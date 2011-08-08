ab8d492042059e3d8bdf2ad35078a1ce78d231e1
unknown <Sajjad@.(none)>
2011-08-08 04:29:44 -0400

    Random background image feature

---
+++ LiveAdhan/LiveAdhan/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveAthan.Controllers
+{
+    public class FileController : Controller
+    {
+        //
+        // GET: /File/
+
+        static Random r = new Random();
+        public ActionResult BackgroundImage()
+        {
+            int whichPicture = r.Next(2);
+            if (whichPicture == 1)
+            {
+                return File(Server.MapPath("~/content/img/bgs/waseef_ramadan.jpg"), "image/jpeg");
+            }
+            else
+            {
+                return File(Server.MapPath("~/content/img/bgs/waseef_ramadan2.jpg"), "image/jpeg");
+            }
+        }
+
+    }
+}
